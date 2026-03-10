e4363e2a9e3be9eb69f51d588766681bb28eafe6
Ruby Kim <rubyjeenkim@gmail.com>
2026-03-10 11:10:14 +0900

    chore: split frontend into separate repo

--- frontend/app/portfolio/ai-chat/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatContainer } from '@/components/chat/ChatContainer';
-
-export default function AIChatPage() {
-    return (
-        <div className="h-[calc(100vh-64px)]">
-            <ChatContainer />
-        </div>
-    );
-}
